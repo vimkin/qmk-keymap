5378b66357861d2be43ff5464b94d9a665421237
malinin <trawl06algebra@icloud.com>
2026-02-14 22:53:15 +0100

    feat: add qwerty/ru layout

--- keyboards/zsa/voyager/keymaps/malinin/keymap.c
+++ keyboards/zsa/voyager/keymaps/malinin/keymap.c
@@ -3,7 +3,7 @@
 #include QMK_KEYBOARD_H
 #include "os_detection.h"
 
-enum layers { BASE, SYMBOLS, MEDIA_NAV, NUM };
+enum layers { BASE, SYMBOLS, MEDIA_NAV, NUM, RU };
 
 enum custom_keycodes {
     SEL_ALL = SAFE_RANGE,
@@ -26,6 +26,12 @@ enum keycode_aliases {
     HRM_N = LT(SYMBOLS, KC_N),
     HRM_S = RCTL_T(KC_S),
 
+    // One-shot mod keys.
+    OSM_LCTL = OSM(MOD_LCTL),
+    OSM_LSFT = OSM(MOD_LSFT),
+    OSM_LALT = OSM(MOD_LALT),
+    OSM_LGUI = OSM(MOD_LGUI),
+
     EMOJI = LCTL(LGUI(KC_SPC)),
 };
 
@@ -34,41 +40,48 @@ enum keycode_aliases {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_voyager(
-    KC_NO,  UNDO_X,   CUT_X,   SEL_ALL,  TD(TD_COPY_PASTE),       KC_GRV,                   LGUI(LSFT(KC_4)), CW_TOGG, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_ESC, KC_Z,     KC_Y,    KC_O,     KC_U,                    KC_EQL,                   KC_Q,             KC_L,    KC_D,  KC_P,  KC_X,  KC_BSPC,
-    KC_W,   HRM_C,    HRM_I,   HRM_A,    HRM_E,                   KC_SCLN,                  KC_K,             HRM_H,   HRM_T, HRM_N, HRM_S, KC_F,
-    KC_NO,  KC_QUOTE, KC_MINS, KC_COMM,  KC_DOT,                  KC_SLASH,                 KC_J,             KC_M,    KC_G,  KC_B,  KC_V,  KC_ENT,
-                                         LT(MEDIA_NAV, KC_SPACE), OSM(MOD_LSFT),            MEH_T(KC_TAB),    LT(NUM, KC_R)
+    KC_NO,  UNDO_X,   CUT_X,   SEL_ALL,  TD(TD_COPY_PASTE),     KC_GRV,              LGUI(LSFT(KC_4)), CW_TOGG, KC_NO, KC_NO, KC_NO, TG(RU),
+    KC_ESC, KC_Z,     KC_Y,    KC_O,     KC_U,                  KC_EQL,              KC_Q,             KC_L,    KC_D,  KC_P,  KC_X,  KC_BSPC,
+    KC_W,   HRM_C,    HRM_I,   HRM_A,    HRM_E,                 KC_SCLN,             KC_K,             HRM_H,   HRM_T, HRM_N, HRM_S, KC_F,
+    KC_NO,  KC_QUOTE, KC_MINS, KC_COMM,  KC_DOT,                KC_SLASH,            KC_J,             KC_M,    KC_G,  KC_B,  KC_V,  KC_ENT,
+                                         LT(MEDIA_NAV, KC_SPC), OSM_LSFT,            MEH_T(KC_TAB),    LT(NUM, KC_R)
+  ),
+  [RU] = LAYOUT_voyager(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+    KC_TRNS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_TRNS,
+    KC_LBRC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+    KC_RBRC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLASH, KC_TRNS,
+                                        KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS
   ),
   [SYMBOLS] = LAYOUT_voyager(
     KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,             KC_NO,   EMOJI,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
     KC_TRNS, KC_GRV,  KC_LABK, KC_RABK,  KC_MINS, KC_PIPE,           KC_CIRC, KC_LCBR, KC_RCBR, KC_DLR,  KC_BSLS,  KC_TRNS,
     KC_NO,   KC_EXLM, KC_ASTR, KC_SLSH,  KC_EQL,  KC_AMPR,           KC_HASH, KC_LPRN, KC_RPRN, KC_SCLN, KC_DQUO,  KC_NO,
-    KC_NO,   KC_TILD, KC_PLUS, KC_COMM,  KC_DOT,  KC_PERC,           KC_AT,   KC_LBRC, KC_RBRC, KC_COLN, KC_QUOTE, KC_TRNS,
-                                         KC_TRNS,  KC_TRNS,          KC_TRNS, KC_TRNS
+    KC_NO,   KC_TILD, KC_PLUS, KC_COMM,  KC_DOT,  KC_PERC,           KC_AT,   KC_LBRC, KC_RBRC, KC_COLN, KC_QUOT, KC_TRNS,
+                                         KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS
   ),
   [MEDIA_NAV] = LAYOUT_voyager(
-    KC_NO,   KC_BRID,       KC_BRIU,       KC_VOLD,       KC_VOLU,        KC_MUTE,          KC_NO,              KC_NO,              KC_NO,   KC_NO,         KC_NO,       KC_NO,
-    KC_TRNS, KC_MSTP,       KC_MPRV,       KC_MNXT,       KC_MPLY,        KC_NO,            KC_PAGE_UP,         LCTL(LSFT(KC_TAB)), KC_UP,   LCTL(KC_TAB),  KC_HOME,     KC_NO,
-    KC_NO,   OSM(MOD_LCTL), OSM(MOD_LSFT), OSM(MOD_LALT), OSM(MOD_LGUI),  MS_BTN1,          KC_PGDN,            KC_LEFT,            KC_DOWN, KC_RIGHT,      KC_END,      KC_NO,
-    KC_NO,   KC_NO,         KC_NO,         KC_NO,         KC_NO,          KC_NO,            KC_MISSION_CONTROL, LGUI(KC_LBRC),      KC_APP,  LGUI(KC_RBRC), DUAL_FUNC_0, KC_NO,
-                                                          KC_TRNS,        KC_TRNS,          KC_TRNS,            KC_TRNS
+    KC_NO,   KC_BRID,  KC_BRIU,  KC_VOLD,  KC_VOLU,  KC_MUTE,          KC_NO,   KC_NO,              KC_NO,   KC_NO,         KC_NO,       KC_NO,
+    KC_TRNS, KC_MSTP,  KC_MPRV,  KC_MNXT,  KC_MPLY,  KC_NO,            KC_PGUP, LCTL(LSFT(KC_TAB)), KC_UP,   LCTL(KC_TAB),  KC_HOME,     KC_NO,
+    KC_NO,   OSM_LCTL, OSM_LSFT, OSM_LALT, OSM_LGUI, MS_BTN1,          KC_PGDN, KC_LEFT,            KC_DOWN, KC_RIGHT,      KC_END,      KC_NO,
+    KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,            KC_MCTL, LGUI(KC_LBRC),      KC_APP,  LGUI(KC_RBRC), DUAL_FUNC_0, KC_NO,
+                                           KC_TRNS,  KC_TRNS,          KC_TRNS, KC_TRNS
   ),
   [NUM] = LAYOUT_voyager(
-    KC_NO,   KC_NO,         KC_NO,         KC_NO,         KC_NO,          KC_NO,            KC_NO,   KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,
-    KC_TRNS, KC_7,          KC_5,          KC_3,          KC_1,           KC_9,             KC_8,    KC_0,          KC_2,          KC_4,          KC_6,          KC_TRNS,
-    KC_NO,   OSM(MOD_LCTL), OSM(MOD_LSFT), OSM(MOD_LALT), OSM(MOD_LGUI),  KC_F11,           KC_F10,  OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LSFT), OSM(MOD_LCTL), KC_NO,
-    KC_NO,   KC_F7,         KC_F5,         KC_F3,         KC_F1,          KC_F9,            KC_F8,   KC_F12,        KC_F2,         KC_F4,         KC_F6,         KC_TRNS,
-                                                          KC_TRNS,        KC_TRNS,          KC_TRNS, KC_TRNS
+    KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,            KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+    KC_TRNS, KC_7,     KC_5,     KC_3,     KC_1,     KC_9,             KC_8,    KC_0,     KC_2,     KC_4,     KC_6,     KC_TRNS,
+    KC_NO,   OSM_LCTL, OSM_LSFT, OSM_LALT, OSM_LGUI, KC_F11,           KC_F10,  OSM_LGUI, OSM_LALT, OSM_LSFT, OSM_LCTL, KC_NO,
+    KC_NO,   KC_F7,    KC_F5,    KC_F3,    KC_F1,    KC_F9,            KC_F8,   KC_F12,   KC_F2,    KC_F4,    KC_F6,    KC_TRNS,
+                                           KC_TRNS,  KC_TRNS,          KC_TRNS, KC_TRNS
   ),
 };
 
 const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT(
   '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*',
+  '*', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', '*',
   'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
-  'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
-  'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R',
-  '*', '*', '*', '*'
+  'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', '*',
+                      'L', 'L', 'R', 'R'
 );
 
 const custom_shift_key_t custom_shift_keys[] = {
